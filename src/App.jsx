f35d34b9d830689f96505a96c9dfb9c0df115bd5
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-10-22 13:52:26 +0530

    This is my first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [todos, setTodo] = useState(0);
+
+
+  useEffect(()=>{
+    axios.get("https://sum-server.100xdevs.com/todos").then(function(response){
+      setTodo(response.data.todos);
+    });
+  },[])
+  return (
+    <>
+       {todos.map(todo=>{<Todo title={todo.title} description={todo.description}/>})}
+    </>
+  )
+}
+
+
+function Todo({title,description}){
+  return <div>
+    <h1>{title}</h1>
+    <p>{description}</p>
+  </div>
+
+}
+export default App
